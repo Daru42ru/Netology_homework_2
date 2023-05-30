fbb496716fc1f921a906418a3280644363782599
Дарья Кротова <daru42ru@gmail.com>
2023-05-30 15:18:28 +0700

    Initial commit

---
+++ src/test/java/TestCard.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestCard {
+    @BeforeEach
+    public void setUpTests() {
+        open("http://localhost:9999/");
+    }
+    @Test
+    public void  shouldSendForm() {
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79998887766");
+        $("[data-test-id=agreement] input").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    public void  shouldSendWithDashInName() {
+        $("[data-test-id=name] input").setValue("Иванов Иван-Николай");
+        $("[data-test-id=phone] input").setValue("+79998887766");
+        $("[data-test-id=agreement] input").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    public void  shouldSendWithLetterE() {
+        $("[data-test-id=name] input").setValue("Иванов Пётр");
+        $("[data-test-id=phone] input").setValue("+79998887766");
+        $("[data-test-id=agreement] input").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    public void  shouldSendWithDashAndSpaceInName() {
+        $("[data-test-id=name] input").setValue("Иванов-Романов Иван Николай");
+        $("[data-test-id=phone] input").setValue("+79998887766");
+        $("[data-test-id=agreement] input").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
